517436b42829bff7da1bd1d89bddb2959ca809d2
NevigChanuka <160095827+NevigChanuka@users.noreply.github.com>
2025-03-06 14:09:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Marketing-Website/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
